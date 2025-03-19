a2eb9788f3c2716a60668b2456795e86ae532e17
HP 830 G5 <psailatur@gmail.com>
2025-03-19 09:18:34 +0700

    Commit Message

--- app/src/main/java/com/example/revisi/MainActivity.java
+++ app/src/main/java/com/example/revisi/MainActivity.java
@@ -2,23 +2,61 @@ package com.example.revisi;
 
 import android.os.Bundle;
 
+import android.os.Bundle;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText NamaBahan, HargaBeli;
+    Spinner spinnerItem;
+    Button btnTambah, btnHitung;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Inisialisasi Komponen
+        NamaBahan = findViewById(R.id.editTextText);
+        HargaBeli = findViewById(R.id.editTextText2);
+        spinnerItem = findViewById(R.id.spinner);
+        btnTambah = findViewById(R.id.button3);
+        btnHitung = findViewById(R.id.button2);
+
+        // Event Klik Tambahkan
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String namaBahan = NamaBahan.getText().toString();
+                String hargaBeli = HargaBeli.getText().toString();
+
+                if (namaBahan.isEmpty() || hargaBeli.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Harap isi semua data!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Bahan Ditambahkan: " + namaBahan, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Event Klik Mulai Hitung
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Menghitung Harga Jual...", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
